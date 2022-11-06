435b4119fed4f35fe06718cde81b30715cca35fe
Holivane <holivane@gmail.com>
2022-11-06 16:37:49 -0300

    método push

---
+++ adiciona-nota.js
@@ -0,0 +1,11 @@
+const notas = [10, 6, 8]
+
+console.log(notas);
+
+notas.push(7);
+
+console.log(notas);
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+console.log('Média = ', media)
